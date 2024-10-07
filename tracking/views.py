09155eedcaa3be371a10db84a281563fa9090384
Arina <bagdaarina@gmail.com>
2024-10-07 16:43:47 +0500

    Get origin and destination from data

--- tracking/views.py
+++ tracking/views.py
@@ -28,10 +28,21 @@ class VehicleLocationView(View):
             
         # Check if there is a unit_id
         if self.unit_id is None:
-            self.courier_id = self.get_courier_id(icao24)
+            self.courier_id, branch, destination_lat, destination_long = self.get_courier_id(icao24)
             self.unit_id = self.get_unit_id(self.courier_id)
             if not self.unit_id:
                 return JsonResponse({"error": "Unit retrieval failed"}, status=500)
+            
+        # Get start location
+        if branch == 'jomiy':
+            start_lat = 41.35556949663072
+            start_long = 69.25377917274001
+        elif branch == 'chinobod':
+            start_lat = 41.35554159796273
+            start_long = 69.30574801224093
+        else: 
+            start_lat = 41.35556949663072    # Default to Jomiy location for now but need to think about this
+            start_long = 69.25377917274001
         
         # Get the last position of the vehicle
         vehicle_info = self.get_last_position(self.session_key, self.unit_id)
@@ -42,7 +53,7 @@ class VehicleLocationView(View):
             # Extracting coordinates
             y = vehicle_info['item']['pos']['y']
             x = vehicle_info['item']['pos']['x']
-            return JsonResponse({"location": {"y": y, "x": x}})
+            return JsonResponse({"location": {"y": y, "x": x, "start_lat": start_lat, "start_long": start_long, "destination_lat":destination_lat, "destination_long":destination_long}})
         else:
             return JsonResponse({"error": "Vehicle not found"}, status=404)
 
@@ -71,8 +82,13 @@ class VehicleLocationView(View):
         response = requests.request("POST", url, headers=headers, data=payload)
 
         if response.ok:
-            courier_id = response.json().get('courier_id')
-            return courier_id
+            delivery_info = response.json()
+            courier_id = delivery_info.get('courier_id')
+            branch = delivery_info.get('branch')
+            branch = branch.replace(" ", "").lower()
+            destination_lat = delivery_info.get('latitude')
+            destination_long = delivery_info.get('longitude')
+            return courier_id, branch, destination_lat, destination_long
         return None
 
     def get_unit_id(self, courier_id):
